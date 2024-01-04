d37b1a2a733691f74947fa1e2aaed1e20040e7bf
Ran <xymsyh@gmail.com>
2024-01-04 20:54:50 +0800

    创建时区与运行状态

--- src/fsaxis/app.py
+++ src/fsaxis/app.py
@@ -15,12 +15,16 @@ class fsaxis(toga.App):
         inp_keyword = toga.TextInput(style=Pack(flex=1))
         inp_content = toga.TextInput(style=Pack(flex=2))
 
+        # 创建时区与运行状态
+        time_zone = toga.TextInput(style=Pack(flex=1))
+        running_status = toga.TextInput(style=Pack(flex=2))
+
         # Creating multi-line input fields
-        inp_line = toga.MultilineTextInput(style=Pack(flex=1, height=100))
-        inp_cell = toga.MultilineTextInput(style=Pack(flex=1, height=200))
+        inp_line = toga.MultilineTextInput(style=Pack(flex=1, height=85))
+        inp_cell = toga.MultilineTextInput(style=Pack(flex=1, height=155))
 
         # Creating buttons
-        button_style = {'width': 85, 'padding_top': 30}
+        button_style = {'width': 85, 'padding_top': 20}
         btn_line = toga.Button('✅line', style=Pack(**button_style))
         btn_cell = toga.Button('✅cell', style=Pack(**button_style))
         btn_clear = toga.Button('清空', style=Pack(**button_style))
@@ -30,11 +34,13 @@ class fsaxis(toga.App):
         row2 = toga.Box(style=row_style, children=[inp_line, btn_line])
         col1 = toga.Box(style=column_style, children=[btn_cell, btn_clear])
         row3 = toga.Box(style=row_style, children=[inp_cell, col1])
+        row4 = toga.Box(style=row_style, children=[time_zone, running_status])
 
         # Adding rows to the main layout box
         main_box.add(row1)
         main_box.add(row2)
         main_box.add(row3)
+        main_box.add(row4)
 
         # Setting up the main window
         self.main_window = toga.MainWindow(title=self.formal_name)
