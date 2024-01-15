91c8d10072ca97f772c9c858de64a6825f354d16
Ran <xymsyh@gmail.com>
2024-01-15 17:32:58 +0800

    1

--- src/fsaxis/app.py
+++ src/fsaxis/app.py
@@ -160,9 +160,13 @@ class fsaxis(toga.App):
         pick_button = toga.Button('选择图片', on_press=self.pick_image, style=Pack(padding=5, flex=1))
         back_button = toga.Button('返回首页', on_press=self.perform_action, style=Pack(padding=5, flex=1))
 
+        action_button2 = toga.Button('上传图片', on_press=self.perform_action2, style=Pack(padding=5, flex=1))
+        pick_button2 = toga.Button('选择图片', on_press=self.pick_image, style=Pack(padding=5, flex=1))
+        back_button2 = toga.Button('返回首页', on_press=self.perform_action, style=Pack(padding=5, flex=1))
 
         # 水平布局的容器，包括动作按钮和文本输入框
         row_1 = toga.Box(style=Pack(direction=ROW, padding=5), children=[action_button, pick_button, back_button])
+        row_2 = toga.Box(style=Pack(direction=ROW, padding=5), children=[action_button2, pick_button2, back_button2])
 
         # 创建 ImageView，用于显示图片
         self.image_view = ImageView(style=Pack(width=400, height=300))
@@ -171,7 +175,7 @@ class fsaxis(toga.App):
         scroll_container = ScrollContainer(content=self.image_view, style=Pack(flex=1))
 
         # 创建垂直布局的容器，包括所有组件
-        box = toga.Box(children=[row_1, scroll_container, row_1], style=Pack(direction=COLUMN, padding=10))
+        box = toga.Box(children=[row_1, scroll_container, row_2], style=Pack(direction=COLUMN, padding=10))
 
         # 更改当前窗口的内容为新布局
         self.main_window.content = box
