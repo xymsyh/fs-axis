4e209343b4996cb5be605022711fe3cfed1cf4c5
Ran <xymsyh@gmail.com>
2024-01-16 15:39:27 +0800

    1

---
+++ src/fsaxis/后删_测试时间.py
@@ -0,0 +1,37 @@
+from datetime import datetime, timedelta
+
+# Current time for reference
+current_time = datetime.now()
+
+# Log data provided by the user
+log_data = [
+    '2024-01-15 22:02:00: 测试\n',
+    '2024-01-15 22:03:19: 测试\n',
+    '2024-01-15 22:03:24: 测试\n',
+    '2024-01-15 22:07:30: 测试\n',
+    '2024-01-15 22:07:33: 测试\n',
+    '2024-01-15 22:07:36: 测试'
+]
+
+# Function to convert time to a relative time string
+def time_ago(time):
+    diff = current_time - time
+    seconds = diff.total_seconds()
+
+    if seconds < 60:
+        return f"{int(seconds)} seconds ago"
+    elif seconds < 3600:
+        return f"{int(seconds // 60)} minutes ago"
+    elif seconds < 86400:
+        return f"{int(seconds // 3600)} hours ago"
+    else:
+        return f"{int(seconds // 86400)} days ago"
+
+# Processing the log data to convert times
+converted_log_data = []
+for entry in log_data:
+    time_str, log_entry = entry.split(': ', 1)
+    log_time = datetime.strptime(time_str, '%Y-%m-%d %H:%M:%S')
+    converted_log_data.append(f"{time_ago(log_time)}: {log_entry}")
+
+print(converted_log_data)
