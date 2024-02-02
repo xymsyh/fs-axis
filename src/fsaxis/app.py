3de6a74321a3ea9446c857324e244394eed03e35
Ran <xymsyh@gmail.com>
2024-02-02 18:26:41 +0800

    调整变量self.clock_icons位置

--- src/fsaxis/app.py
+++ src/fsaxis/app.py
@@ -140,6 +140,14 @@ class fsaxis(toga.App):
         self.my_global_variable = None
         self.history_layout_box = None
         self.used_data_json = None
+        self.clock_icons = {
+            0: '🕛', 0.5: '🕧', 1: '🕐', 1.5: '🕜',
+            2: '🕑', 2.5: '🕝', 3: '🕒', 3.5: '🕞',
+            4: '🕓', 4.5: '🕟', 5: '🕔', 5.5: '🕠',
+            6: '🕕', 6.5: '🕡', 7: '🕖', 7.5: '🕢',
+            8: '🕗', 8.5: '🕣', 9: '🕘', 9.5: '🕤',
+            10: '🕙', 10.5: '🕥', 11: '🕚', 11.5: '🕦',
+        }
 
         threading.Thread(target=self.compare_keyword_data).start()
 
@@ -149,14 +157,7 @@ class fsaxis(toga.App):
     # @staticmethod
     def get_clock_icon(self, hour, minute):
         # 根据小时和分钟确定图标
-        self.clock_icons = {
-            0: '🕛', 0.5: '🕧', 1: '🕐', 1.5: '🕜',
-            2: '🕑', 2.5: '🕝', 3: '🕒', 3.5: '🕞',
-            4: '🕓', 4.5: '🕟', 5: '🕔', 5.5: '🕠',
-            6: '🕕', 6.5: '🕡', 7: '🕖', 7.5: '🕢',
-            8: '🕗', 8.5: '🕣', 9: '🕘', 9.5: '🕤',
-            10: '🕙', 10.5: '🕥', 11: '🕚', 11.5: '🕦',
-        }
+        
         # 计算对应的键值
         key = hour % 12 + (0.5 if minute >= 30 else 0)
         # 返回对应的图标
